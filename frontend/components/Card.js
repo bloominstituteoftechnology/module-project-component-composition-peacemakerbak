dd652e9a4a032cf7a627e7202d5f03bc740df968
peacemakerbak <peacemakerbak@gmail.com>
2024-04-03 17:14:34 -0400

    styled p + h1

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,20 +1,38 @@
 import React from 'react'; 
 import styled from 'styled-components';
 
-const styledCard = styled.div`
+const StyledCard = styled.div`
+  border: 1px solid black;
+  border-radius: 12px;
+  padding: 10px;
 
+  h2 {
+    color: grey;
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+
+    &:hover {
+      color: black;
+    }
+  }
+
+  p {
+    font-size: 1rem;
+    margin-bottom: 10px;
+    color: darkgray;
+  }
 `
 
 
 export default function Card ({title, text, image}) { // one react component: three props: title, text, and image
     return (
-      <div className="card">
+      <StyledCard className="card">
           <h2>{title}</h2>
           <p> {text}</p>
           <figure> 
             <img src={image} />
             <figcaption>Photo by NASA!! </figcaption>
           </figure>
-        </div>
+        </StyledCard>
     )
   }
