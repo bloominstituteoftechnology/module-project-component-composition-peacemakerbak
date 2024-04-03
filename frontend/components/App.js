fe8a6c8c3985a4dbbccbb5d2545001d9831b7995
peacemakerbak <peacemakerbak@gmail.com>
2024-04-03 15:03:25 -0400

    almost done - wifi out lol

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,12 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const API_KEY = "DEMO_KEY";
+const URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+
+function Card ({title, text, image}) { // one react component: three props: title, text, and image
   return (
-    <p>
-      Read through the instructions in the README.md file to build your NASA
-      app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-    </p>
+    <div className="card">
+        <h2>{title}</h2>
+        <p> {text}</p>
+        <figure>
+          <img src={image} />
+          <figcaption>Photo by NASA!! </figcaption>
+        </figure>
+      </div>
   )
 }
 
-export default App
+function App() {
+  // function is for fetching the photo from the NASA API
+  const [photo, setPhoto] = useState();
+  useEffect(() => {
+    // useEffect is a hook that runs after the first render of the component
+
+    function fetchPhoto() {
+      // fetchPhoto function is for fetching the photo from the NASA API
+      axios // axios is used to make HTTP requests
+        .get(URL) // get request to the NASA API
+        .then((response) => {
+          // then method is used to handle the response from the API
+          setPhoto(response.data);
+          console.log(response.data); // log the response data to the console so I can see the data
+        })
+        .catch((error) => {
+          // catch method is used to handle any errors that occur during the request
+          console.log(error.message); // log the error message to the console so I can see what went wrong
+        });
+    }
+    //fetchPhoto();
+    setPhoto({
+      title: "Astronomy Picture of the Day",
+      url: "https://apod.nasa.gov/apod/image/2404/Pa30V_NASA_960.jpg",
+      explanation:
+        "This is a picture of NGC 2174 taken by the Hubble Space Telescope.",
+    });
+  }, []);
+
+  if (!photo) {
+    return "Fetching photo...";
+  } // if there is no photo, return a loading message
+  
+  
+  return ( 
+    <section>
+      <Card
+      title={(photo.title)}
+      text={(photo.explanation)}
+      image={(photo.url)}
+      />
+    </section>
+  )
+}
+
+export default App;
+
+
